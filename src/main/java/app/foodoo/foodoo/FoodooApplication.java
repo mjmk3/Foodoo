cae0891fed2baa0708e283e5d10fae28ee59ab43
Mohamad J. Makki <malanbaki@transforat.com>
2023-01-13 21:14:50 +0300

    Starting Back-End

---
+++ src/main/java/app/foodoo/foodoo/FoodooApplication.java
@@ -0,0 +1,13 @@
+package app.foodoo.foodoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/foodoo/foodoo/FoodooApplicationTests.java
@@ -0,0 +1,13 @@
+package app.foodoo.foodoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
